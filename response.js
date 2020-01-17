83926f5258ca4a3a133559500d8e3c426dab55e8
Abhishek Doshi <abhishekdoshi94@gmail.com>
2020-01-17 15:21:08 +0530

    Display change

--- response.js
+++ response.js
@@ -8,6 +8,7 @@ function createBox(){
     square.setAttribute("id","square");
     text.setAttribute("id","text"); 
     box.appendChild(square);
+    square.innerHTML = '<font size="5">Click Here!</font>';
     document.body.appendChild(box);
     document.body.appendChild(text);
     square.addEventListener('click', handleClick);
@@ -25,15 +26,15 @@ function handleClick(){
         setTimeout(changeAttr,1500);
     }
     else {
-        text.innerHTML = "Game Over Final Score: " + score;
+        text.innerHTML = "Your Response time is more than 2.5 i.e "+response+"<br>"+" Final Score:"+ score;
         square.removeEventListener('click', handleClick);
     }
 }
 
 function changeAttr(){
     square.style.display = "block";
-    square.style.top = Math.floor(Math.random()*550)+'px';
-    square.style.left = Math.floor(Math.random()*750)+'px';
+    square.style.top = Math.floor(Math.random()*540) +'px';
+    square.style.left = Math.floor(Math.random()*740) +'px';
 }
 
 createBox();
